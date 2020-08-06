3fba16de0290d02915ac1bd9127f65b4492e8757
raven4ever <wrtv23@gmail.com>
2020-08-06 20:01:04 +0300

    initial backend

---
+++ backend/src/main/java/com/adrian/randombackendapp/RandombackendappApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.randombackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandombackendappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandombackendappApplication.class, args);
+    }
+
+}
